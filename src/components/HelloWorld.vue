0d99eb1a7d6a03a6dbe08e351bda73e57be8395d
LoopMirror <plasticfish@foxmail.com>
2024-05-09 15:24:57 +0800

    update

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+
+<script>
+import {HomeApi} from "../request/api";
+export default {
+  data () {
+    return {
+
+ 
+    }
+  },
+  created(){
+    HomeApi().then(res=>{
+      console.log(res);
+    })
+  }
+}
+</script>
+ 
+<style lang = "less" scoped>
+  
+</style>
